478eb5c1f4ec647314d9f1c378684576c5504e9c
Aerossky <riskyradeonrx560@gmail.com>
2022-10-24 22:12:38 +0700

    css link

--- resources/views/layouts/mainlayouts.blade.php
+++ resources/views/layouts/mainlayouts.blade.php
@@ -13,14 +13,14 @@
         <meta name="viewport"
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Kuliah | @yield('title')</title>
+        <title>PAPA JOE'S | @yield('title')</title>
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="mydesign/mystyle.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 
-<body>
+<body class="">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Navbar</a>
