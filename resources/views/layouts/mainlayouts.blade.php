2123e82aa35f33d1e840e931a38cdf4a6e9180a0
JosephWijaya34 <92154348+JosephWijaya34@users.noreply.github.com>
2022-10-24 23:06:53 +0700

    hero section

--- resources/views/layouts/mainlayouts.blade.php
+++ resources/views/layouts/mainlayouts.blade.php
@@ -21,34 +21,37 @@
     </head>
 
 <body class="">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark navBlur">
         <div class="container-fluid">
-          <a class="navbar-brand logo" href="#">PAPA JOE'S</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/menu">Menu</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto" href="#">Service</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto" href="#">Contact</a>
-                </li>
-            </ul>
-            
-          </div>
+            <a class="navbar-brand logo" href="#">PAPA JOE'S</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link hoverNav" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link scrollto hoverNav" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link hoverNav" href="/menu">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link scrollto hoverNav" href="#">Service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link scrollto hoverNav" href="#">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
         </div>
-      </nav>
+    </nav>
+    <div class="hero"></div>
     <div class="container">
         @yield('content')
     </div>
